a497384ee9ba9cf7ce9b3f4456084aca4efabc47
Usman Kamal <usman20kamal@gmail.com>
2023-12-08 23:27:07 +0500

    image upload funtionality added

---
+++ client/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "transparent-69009.firebaseapp.com",
+  projectId: "transparent-69009",
+  storageBucket: "transparent-69009.appspot.com",
+  messagingSenderId: "1045906455625",
+  appId: "1:1045906455625:web:27bcb821840e0853c89398",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
