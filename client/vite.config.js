d3ae8cfc247107992131952269b5b2f9055e50a6
Ameera Haider Rizvi <77534392+ameerahaider@users.noreply.github.com>
2024-06-10 22:24:50 +0500

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    host: '0.0.0.0', // Listen on all network interfaces
+    port: 5174, // Port used by Vite server
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: process.env.BACKEND_TARGET || 'http://localhost:3000',
         secure: false,
       },
     },
